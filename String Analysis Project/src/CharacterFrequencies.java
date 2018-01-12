b11d968dc9fc6ca9d53ebad26fb88eef221346d8
Lydia Gray <lydia.gray@stockport.gov.uk>
2018-01-12 15:51:43 +0000

    Added numberFrequencies method and display method

--- String Analysis Project/src/CharacterFrequencies.java
+++ String Analysis Project/src/CharacterFrequencies.java
@@ -2,7 +2,11 @@
 public class CharacterFrequencies {
 	
 	static final String alphabet = "abcdefghijklmnopqrstuvwxyz";
+	static final char[] charAlphabet = alphabet.toCharArray();
 	static final String nonAlphaNumeric = ".,!?:;\"'&()-@\\/";
+	static final char[] charNonAlphaNumeric = nonAlphaNumeric.toCharArray();
+	static final String numbers = "0123456789";
+	static final char[] charNumbers = numbers.toCharArray();
 	
 	public static int countCharacters(String str, char character) {
 		String newStr = str.toLowerCase();
@@ -36,6 +40,27 @@ public class CharacterFrequencies {
 		}		
 		return frequencyValues;
 	}
+	
+	public static int[] numberFrequencies(String str) {
+		int[] numberFrequencyValues = new int[10];
+		char[] charNumbers = numbers.toCharArray();
+		
+		for (int i = 0; i < charNumbers.length; i++) {
+			numberFrequencyValues[i] = countCharacters(str, charNumbers[i]); 
+		}		
+		return numberFrequencyValues;
+	}
+	
+	public static void displayFrequencies(int[] frequencyValues, char[] characterArray) {
+		System.out.format("+---------+---------+\r\n");
+		System.out.format("|Character|Frequency|\r\n");
+		System.out.format("+---------+---------+\r\n");
+		for (int i = 0; i < frequencyValues.length; i++) {
+			System.out.format("| %-7s | %-7d |\r\n", characterArray[i], frequencyValues[i]);
+		}
+		System.out.format("+---------+---------+\r\n");
+	}
+
 
 }
 
